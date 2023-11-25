6ea551af507a7114ae34f5031efd13275f670914
Nabi Karampoor <thisisnabi@outlook.com>
2023-11-25 22:47:08 +0330

    feat: Add Aggregate root test case

---
+++ test/Devblogs.Blogger.UnitTests/BuildingBlocks/AggregateRootTests.cs
@@ -0,0 +1,36 @@
+﻿namespace Devblogs.Blogger.UnitTests.BuildingBlocks;
+
+public class AggregateRootTests
+{
+    [Fact]
+    public void AddsDomainEventToAggregateRoot()
+    {
+        // Arrange
+        var Id = Random.Shared.Next(int.MaxValue);
+        var entity = new User(Id);
+
+        // Act
+        entity.AddProfileUpdatedEvent();
+
+        // Assert
+        entity.Events.Should().HaveCount(1);
+        entity.Events.Should().AllBeOfType<ProfileUpdatedEvent>();
+    }
+}
+
+
+public class ProfileUpdatedEvent : DomainEventBase { }
+
+public class User : AggregateRoot<long>
+{
+    public User(int id)
+    {
+        Id = id;
+    }
+
+    public void AddProfileUpdatedEvent()
+    {
+        var domainEvent = new ProfileUpdatedEvent();
+        RegisterEvent(domainEvent);
+    }
+}
