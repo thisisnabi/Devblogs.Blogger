cd2790d1e41f5cae4255e65bef5c2383b1483f99
Nabi Karampoor <thisisnabi@outlook.com>
2023-11-25 22:11:02 +0330

    feat: Add entity base in building blocks

---
+++ src/Devblogs.Blogger.Domain/Common/EntityBase.cs
@@ -0,0 +1,32 @@
+﻿namespace Devblogs.Blogger.Domain.Common;
+
+public abstract class EntityBase<TKey>
+    where TKey : struct 
+{
+    public TKey Id { get; protected set; }
+
+    public override bool Equals(object? obj)
+    {
+        var entity = obj as EntityBase<TKey>;
+
+        if(entity is null) return false;
+
+        return GetType() == entity.GetType() &&
+               EqualityComparer<TKey>.Default.Equals(Id, entity.Id);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(GetType(), Id);
+    }
+
+    public static bool operator ==(EntityBase<TKey> leftEntity, EntityBase<TKey> rightEntity)
+    {
+        return EqualityComparer<EntityBase<TKey>>.Default.Equals(leftEntity, rightEntity);
+    }
+
+    public static bool operator !=(EntityBase<TKey> leftEntity, EntityBase<TKey> rightEntity)
+    {
+        return !(leftEntity == rightEntity);
+    }
+}
